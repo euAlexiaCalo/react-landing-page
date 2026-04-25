b789138b4d2da86f16434120490e3661ffb604f4
Aléxia Caló <alexiacalovj@outlook.com>
2026-04-25 01:36:02 -0300

    feat: componente Footer da plataforma

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import "./Footer.css";
+
+function Footer() {
+    return (
+        <footer>
+            <div className="footer-container container">
+                <p>EduTrack</p>
+                <ul>
+                    <li>Privacy Policy</li>
+                    <li>Terms of Service</li>
+                    <li>Documentation</li>
+                    <li>Support</li>
+                </ul>
+                <p>© 2024 EduTrack Technical Systems. All rights reserved.</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
